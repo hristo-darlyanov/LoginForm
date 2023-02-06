f8b69d4025bdc6d9c6bf00e7e78684744592cfa2
it.pgee.burgas <it.pgee.burgas@gmail.com>
2023-02-06 12:11:23 +0200

    Add project files.

---
+++ LoginForm/Controller/LoginController.cs
@@ -0,0 +1,22 @@
+﻿using LoginForm.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginForm.Controller
+{
+    internal class LoginController
+    {
+        public List<User> GetAllUsers()
+        {
+            List<User> users = new List<User>()
+            {
+                new User("user1", "123"),
+                new User("user2", "321")
+            };
+            return users;
+        }
+    }
+}
---
+++ LoginForm/View/LoginView.cs
@@ -0,0 +1,60 @@
+﻿using LoginForm.Controller;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoginForm.View
+{
+    public partial class LoginView : Form
+    {
+        LoginController loginController = new LoginController();
+        public LoginView()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string username = txtUsername.Text;
+            string password = txtPassword.Text;
+            var users = loginController.GetAllUsers();
+            bool correctUsername = false;
+            bool correctPassword = false;
+            foreach (var item in users)
+            {
+                if (username == item.Username)
+                {
+                    correctUsername = true;
+                    if (password == item.Password)
+                    {
+                        correctPassword = true;
+                    }
+                }
+            }
+
+            if (correctUsername)
+            {
+                if (correctPassword)
+                {
+                    MainPageView mainpage = new MainPageView();
+                    this.Hide();
+                    mainpage.Show();
+                }
+                else
+                {
+                    MessageBox.Show("Wrong Password");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Wrong username");
+            }
+        }
+    }
+}
